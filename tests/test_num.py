150800b9a3007e0868ce850e974879cdb4e6cc1b
Michael Kim <mkim0407@gmail.com>
2019-04-14 17:26:02 -0700

    add numbers

---
+++ tests/test_num.py
@@ -0,0 +1,27 @@
+from logical.num import *
+
+
+def test_div():
+    div3 = DivBy(3)
+    assert [
+               div3(x)
+               for x in range(15)
+           ] == [True, False, False] * 5
+
+
+def test_even_odd():
+    assert [
+               is_even(x)
+               for x in range(10)
+           ] == [True, False] * 5
+
+    assert [
+               is_odd(x)
+               for x in range(10)
+           ] == [False, True] * 5
+
+
+def test_integer():
+    assert is_integer(1)
+    assert is_integer(1.0)
+    assert not is_integer(1.1)
