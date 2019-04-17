1142ac43d54dd8fcd7b5fcc9f181df9ed8700098
Michael Kim <mkim0407@gmail.com>
2019-04-17 12:22:44 -0700

    expose BaseFunction in library for typing and subclassing

--- logical/__init__.py
+++ logical/__init__.py
@@ -1,5 +1,5 @@
 try:
-    from ._base import Function
+    from ._base import BaseFunction, Function
 except ImportError:  # pragma: no cover
     # setup.py imports __version__, at which point dependencies in _base may not be installed
     pass
